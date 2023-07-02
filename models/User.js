1ab6fc667b9c8cffdac10046037bf1acd2bcd433
Tisha Dubey <76221139+Tishadubey01@users.noreply.github.com>
2023-07-02 16:49:19 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -10,6 +10,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  isSubscribed: {
+    type: Boolean,
+    default: false, // Set the default value to false if not provided
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
